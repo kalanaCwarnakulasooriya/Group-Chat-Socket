6d62a718b31c860feea2e03288b5909d35160855
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-05-21 19:26:02 +0530

    Login Controller

--- src/main/java/lk/ijse/groupchat/controller/LoginController.java
+++ src/main/java/lk/ijse/groupchat/controller/LoginController.java
@@ -2,21 +2,42 @@ package lk.ijse.groupchat.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
+    public static String userName = "";
+
+    public AnchorPane rootNode;
 
     @FXML
     private TextField txtName;
 
     @FXML
-    void btnEnterClickOnAction(ActionEvent event) {
+    void btnEnterClickOnAction(ActionEvent event) throws IOException {
+        userName = txtName.getText();
+        if (txtName.getText().isEmpty()) {
+            new Alert(Alert.AlertType.WARNING, "Please Enter Your Name").show();
+            return;
+        }
 
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginController.class.getResource("../view/Client.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage)rootNode.getScene().getWindow();
+        stage.setTitle("Login to Chat");
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
-    void txtNameOnAction(ActionEvent event) {
-
+    void txtNameOnAction(ActionEvent event) throws IOException {
+        btnEnterClickOnAction(event);
     }
 
 }
